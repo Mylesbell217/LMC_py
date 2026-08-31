4fb1215c112817239bbdf6531d0b6e2dc3c0931c
Mylesbell217 <mylesb2007@gmail.com>
2026-08-31 22:02:39 +0100

    working on command line args

--- asm/main.py
+++ asm/main.py
@@ -5,20 +5,27 @@ import sys
 import lex
 
 #gets the file name to be passed into assembler
-#TODO: ensure file name ends in .txt (change to .s later)
-def get_file_name() -> str:
-    return sys.argv[1]
+def get_file_name():
+    return str(sys.argv[1])
+
 
 if __name__ == "__main__":
-    timer = time.time()     #times compilation
+    start_time = time.perf_counter()     #compilation timer
 
     #lex.read_file(get_file_name())
     #print(lex.parse_inst("SUB"))
     #lex.decode_line("LOOP   LDA A")
     file_name = get_file_name()
 
-    #TODO: use the read_file function to pass the file text into decode_line 
-    lex.decode_line(file)
+    #TODO: use the read_file function to pass the file text into decode_line
+
+    #testing reading from file
+    #TODO: ensure file name ends in .txt (change to .s later)
+    try:
+        with open(file_name) as file:
+            lex.decode_line(file.readline())
+    except FileNotFoundError:
+        print("Must be a valid .txt file")
 
 
-    print(f'execution took {(time.time() - timer)*1000} ms')
+    print(f'execution took {(time.perf_counter() - start_time)} s')
